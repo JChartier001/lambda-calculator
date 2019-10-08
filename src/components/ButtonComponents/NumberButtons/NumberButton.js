368adc6f6e421eea210223618560cb9118f40c24
JChartier001 <JChartier001@gmail.com>
2019-10-08 14:23:00 -0500

    Attempting to import numbers array to numberbutton.js

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
 import React from "react";
+// import {Numbers} from "../NumberButtons/Numbers"
+// import {numbers} from "../../../data"
 
-const NumberButton = () => {
+ const NumberButton = (props) => {
+  console.log(props);
   return (
-    <>
+    <button className = "numberButton">
+      
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.number}
+    </button>
   );
 };
+
+export default NumberButton;
